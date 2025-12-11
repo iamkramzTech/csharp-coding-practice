c2d73d16244581b139f7bcec8eeb98a94405ee7d
iamkramzTech <83441254+iamkramzTech@users.noreply.github.com>
2025-12-11 23:36:25 +0800

    done loop for count, and even numbers. but needed to refactor to different methods

---
+++ Day03Loops/Day03Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day03Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countStart = 1;
+            Console.Write("Enter a Number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"FOR LOOP: Counting from {countStart} to {number}");
+            for (countStart = 1; countStart <= number; countStart++)
+            {
+                Console.Write(countStart + " ");
+            }
+            int countStartWhile = 1;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"WHILE LOOP: Even numbers from {countStartWhile} to {number}");
+            while (countStartWhile <= number)
+            {
+                if (countStartWhile % 2 == 0)
+                {
+                    Console.Write(countStartWhile + " ");
+                }
+               
+                countStartWhile += 1;
+            }
+
+           
+        }
+    }
+}
